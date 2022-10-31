eb28e02acf1b211199aee2a3de5fb409660ffa18
hankangwen <13538898378@163.com>
2022-10-31 15:24:41 +0800

    start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+python3 ./csdn_crawler.py
+
+year=`date +%Y `
+month=`date +%m `
+day=`date +%d `
+hour=`date +%H`
+min=`date +%M`
+now=$year-$month-$day-$hour-$min
+
+
+git config --global user.email "13538898378@163.com"
+git config --global user.name "hankangwen"
+
+git add .
+git commit -m "$now"
