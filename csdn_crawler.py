3bf72814a5142250db079b411d097b741caeaad1
hankangwen <46420877+hankangwen@users.noreply.github.com>
2022-11-01 18:39:50 +0800

    Update csdn_crawler.py

--- csdn_crawler.py
+++ csdn_crawler.py
@@ -8,37 +8,26 @@ headers = {
 }
 
 # test start
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127485638', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127622414', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/106072568', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127623802', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/104053265', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/109255357', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/104036671', headers=headers)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/118550011', headers=headers)
-
+headers0 = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36(KHTHL, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+}
 headers1 = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
 }
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127485638', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127622414', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/106072568', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/127623802', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/104053265', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/109255357', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/104036671', headers=headers1)
-requests.get('https://blog.csdn.net/qq_34035956/article/details/118550011', headers=headers1)
 
-html_file = requests.get('https://blog.csdn.net/qq_34035956?type=blog', headers=headers1)
-obj_soup = bs4.BeautifulSoup(html_file.text, 'lxml')
-result = []
-for link in obj_soup.find_all('a'):  # 遍历网页中所有的超链接（a标签）
-    result.append("{}".format(link.get('href')))
-
-for link in result:
-    if link.find('category_') != -1:
-        requests.get(link, headers=headers)
-        requests.get(link, headers=headers1)
+url0 = 'https://blog.csdn.net/qq_34035956?type=blog'
+html_file0 = requests.get(url0, headers=headers0)
+obj_soup0 = bs4.BeautifulSoup(html_file0.text, 'lxml')
+for link in obj_soup0.find_all('a'):  # 遍历网页中所有的超链接（a标签）
+    value0 = "{}".format(link.get('href'))
+    if value0.find('category_') != -1:
+        html_file1 = requests.get(value0, headers=headers0)
+        obj_soup1 = bs4.BeautifulSoup(html_file1.text, 'lxml')
+        for link1 in obj_soup1.find_all('a'):
+            value1 = "{}".format(link1.get('href'))
+            if value1.find('qq_34035956/article') != -1 & value1.find("comments") == -1:
+                requests.get(value1, headers=headers0)
+#                 requests.get(value1, headers=headers1)
         
 # test end
 
