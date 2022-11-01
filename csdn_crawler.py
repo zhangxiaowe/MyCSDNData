be1ed2620803c98021e7f743af117d3256bef86a
hankangwen <46420877+hankangwen@users.noreply.github.com>
2022-11-01 10:24:45 +0800

    Update csdn_crawler.py

--- csdn_crawler.py
+++ csdn_crawler.py
@@ -7,14 +7,30 @@ headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36(KHTHL, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
 }
 
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/127485638', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/127622414', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/106072568', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/127623802', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/104053265', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/109255357', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/104036671', headers=headers)
-html_file0 = requests.get('https://blog.csdn.net/qq_34035956/article/details/118550011', headers=headers)
+-- test start
+requests.get('https://blog.csdn.net/qq_34035956/article/details/127485638', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/127622414', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/106072568', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/127623802', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/104053265', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/109255357', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/104036671', headers=headers)
+requests.get('https://blog.csdn.net/qq_34035956/article/details/118550011', headers=headers)
+
+headers1 = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+}
+html_file = requests.get('https://blog.csdn.net/qq_34035956?type=blog', headers=headers1)
+obj_soup = bs4.BeautifulSoup(html_file.text, 'lxml')
+result = []
+for link in obj_soup.find_all('a'):  # 遍历网页中所有的超链接（a标签）
+    result.append("{}".format(link.get('href')))
+
+for link in result:
+    if link.find('category_') != -1:
+        requests.get(link, headers=headers1)
+        
+-- test end
 
 html_file = requests.get(url, headers=headers)
 obj_soup = bs4.BeautifulSoup(html_file.text, 'lxml')
